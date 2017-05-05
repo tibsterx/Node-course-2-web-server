e9cabb0c08c8fdff9e41b14a674ce46dcb629232
DESKTOP-4ISDEOC\t_bur <t_burns10@hotmail.com>
2017-05-04 21:04:29 -0600

    'addprojectpage'

--- server.js
+++ server.js
@@ -49,6 +49,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'You suck boy!'
